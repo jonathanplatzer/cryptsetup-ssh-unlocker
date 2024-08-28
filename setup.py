3f0ad17bd537941da48769b7807d2ca40e027001
Jonathan Platzer <platzerjonathan@gmail.com>
2024-08-28 14:03:41 +0200

    Update to asyncssh 2+ and python 3.6+

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from unlocker import __version__
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-if sys.version_info[:2] < (3, 5):
-    sys.exit('ssh-unlocker requires Python 3.5 or higher, sorry. Please consider using `pyenv` if your distribution does not provide Python 3.5+')
+if sys.version_info[:2] < (3, 6):
+    sys.exit('ssh-unlocker requires Python 3.6 or higher, sorry. Please consider using `pyenv` if your distribution does not provide Python 3.6+')
 
 setup(
     name='ssh-unlocker',
@@ -27,7 +27,7 @@ setup(
     url='https://github.com/ViktorStiskala/cryptsetup-ssh-unlocker',
     author='Viktor Stískala',
     author_email='viktor@stiskala.cz',
-    install_requires=['asyncssh>=1.11,<1.13', 'bcrypt>=3.1'],
+    install_requires=['asyncssh>=2,<3', 'bcrypt>=3.1'],
     classifiers=[
         'Intended Audience :: System Administrators',
         'Operating System :: OS Independent',
